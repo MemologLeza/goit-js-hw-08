eba48699ea45f263bfacf911224437e6a1274d3e
MemologLeza <121899131+MemologLeza@users.noreply.github.com>
2023-06-04 22:20:06 +0300

    task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector(".feedback-form");
+const feedbackFormState = {
+        email:"",
+        message:""
+};
+ onUpdatePage();
+feedbackForm.addEventListener('submit', OnFormSubmit);
+feedbackForm.addEventListener('input', throttle(OnFormInput, 500));
+
+function OnFormInput(evt) {
+    
+    feedbackFormState.email = feedbackForm.email.value;
+    feedbackFormState.message = feedbackForm.message.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+};
+   
+function OnFormSubmit(evt) {
+    evt.preventDefault();
+   console.log(feedbackFormState);
+    feedbackForm.reset();
+    localStorage.removeItem("feedback-form-state");
+}
+
+function onUpdatePage() {
+    const saveFeedbackFormState = localStorage.getItem("feedback-form-state");
+    let parseSaveFeedbackFormState = "";
+    try {
+        parseSaveFeedbackFormState = JSON.parse(saveFeedbackFormState);
+    } catch (error) {
+        console.log(error.name);
+        console.log(error.message);
+    };
+    if (parseSaveFeedbackFormState) {
+         feedbackForm.email.value = parseSaveFeedbackFormState.email? parseSaveFeedbackFormState.email:"";
+         feedbackForm.message.value = parseSaveFeedbackFormState.message? parseSaveFeedbackFormState.message:"";
+    }
+//     feedbackForm.email.value = parseSaveFeedbackFormState.email;
+//    feedbackForm.message.value = parseSaveFeedbackFormState.message;
+};
