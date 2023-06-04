2b222c6faeffc9df9bc7b12cffbe00f41c156321
MemologLeza <121899131+MemologLeza@users.noreply.github.com>
2023-06-04 18:57:48 +0300

    task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+    
+
+const playerCurrentTime = localStorage.getItem("videoplayer-current-time")
+player.setCurrentTime(playerCurrentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
+
+const onPlayGetCurrentTime = function(data) {
+     player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+    }).catch(function (error) {
+    });
+};
+
+player.on('timeupdate', throttle(onPlayGetCurrentTime, 1000));
